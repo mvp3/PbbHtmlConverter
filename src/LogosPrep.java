bc983da443eede723d1fa54aa8213fdcbce89b90
mvp3 <manuel@pereirasplace.com>
2014-08-14 12:11:57 -0700

    New version

--- src/LogosPrep.java
+++ src/LogosPrep.java
@@ -42,7 +42,7 @@ public class LogosPrep {
 	 */
 	public static void main(String[] args) throws IOException 
 	{
-		System.out.println("Starting PBB HTML parser version 1.2");
+		System.out.println("Starting PBB HTML parser version 1.3");
     	LogosPrep lp = new LogosPrep();
 	    if ( args[0].endsWith( ".html" ) ) {
 	    	lp.prepareFile( args[0] );
@@ -74,6 +74,39 @@ public class LogosPrep {
 		Elements links = null;
 		
 		/*
+		 * Process Headers first
+		 */
+		btags = 0;
+		Elements headers = doc.getElementsByTag("h1");
+	    for ( Element header : headers )
+	    {
+	    	if (header.text().equalsIgnoreCase("preface") ||
+	    		header.text().equalsIgnoreCase("introduction") ) {
+	    		header.tagName("h2");
+	    	} else {
+		    	header.before( "{{field-on:heading}}" );
+		    	header.after( "{{field-off:heading}}" );
+		    	btags++;
+	    	}
+	    }
+	    if (btags > 0 ) System.out.println( btags + " (H1) headings tagged.");
+		
+        btags = 0;
+        headers = doc.getElementsByTag("h2");
+        for ( Element header : headers )
+        {
+	    	if (header.text().equalsIgnoreCase("preface") ||
+		    	header.text().equalsIgnoreCase("introduction") ) {
+	    		// Skip
+	    	} else {
+		    	header.before( "{{field-on:heading}}" );
+		    	header.after( "{{field-off:heading}}" );
+	        	btags++;
+	    	}
+        }
+        if (btags > 0 ) System.out.println( btags + " (H2) headings tagged.");
+		
+		/*
 		 * Parse raw HTML output from WS
 		 */
 		ArrayList<String> missed = new ArrayList<String>();
@@ -90,11 +123,9 @@ public class LogosPrep {
 				
             } else if ( span.attr("class").equalsIgnoreCase("trans-grc") || 
                         span.attr("class").equalsIgnoreCase("trans-heb") ||
-                        span.attr("class").equalsIgnoreCase("trans-arc") ||
+                        span.attr("class").startsWith("trans-") 		 ||
                         span.attr("class").equalsIgnoreCase("gloss")     ||
-                        span.attr("class").equalsIgnoreCase("lang-heb")  ||
-                        span.attr("class").equalsIgnoreCase("lang-lat")  ||
-                        span.attr("class").equalsIgnoreCase("lang-ger") ) {
+                        span.attr("class").startsWith("lang-") ) {
               String txt = span.text();
               Element e = new Element(Tag.valueOf("i"), "");
               e.text(txt);
@@ -117,34 +148,112 @@ public class LogosPrep {
 			}
 		}
 		if (btags > 0 ) System.out.println(btags + " commentary tags inserted.");
-        if ( missed.size() > 0 ) System.out.println("  SPAN classes not handled: " + missed.toString() );
-		
-		btags = 0;
-		Elements headers = doc.getElementsByTag("h1");
-	    for ( Element header : headers )
-	    {
-	      header.before( "{{field-on:heading}}" );
-	      header.after( "{{field-off:heading}}" );
-	      btags++;
-	    }
-	    if (btags > 0 ) System.out.println( btags + " (H1) headings tagged.");
+        if ( missed.size() > 0 ) System.out.println("  <SPAN> classes not handled: " + missed.toString() );
 		
         btags = 0;
-        headers = doc.getElementsByTag("h2");
-        for ( Element header : headers )
-        {
-          header.before( "{{field-on:heading}}" );
-          header.after( "{{field-off:heading}}" );
-          btags++;
-        }
-        if (btags > 0 ) System.out.println( btags + " (H2) headings tagged.");
-
+		missed = new ArrayList<String>();
+		ps = doc.getElementsByTag("p");
+		for ( Element p : ps ) {
+			if ( p.attr("class").equalsIgnoreCase("booktitle") ) {
+				String txt = p.text();
+	            Element e = new Element(Tag.valueOf("h1"), "");
+	            e.text(txt);
+	            e.attr("id", "title");
+	            p.replaceWith(e);
+	            e.before("{{field-on:title}}");
+	            e.after("{{field-off:title}}");
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("subtitle") ) {
+				String txt = p.text();
+	            Element e = new Element(Tag.valueOf("h1"), "");
+	            e.text(txt);
+	            e.attr( "id", "subtitle" );
+            	e.attr( "style", "font-size:smaller;" );
+	            p.replaceWith(e);
+	            e.before("{{field-on:subtitle}}");
+	            e.after("{{field-off:subtitle}}");
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("byline") ) {
+            	p.attr( "style", "text-align:center;" );
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("author") ) {
+	            p.attr( "id", "author" );
+            	p.attr( "style", "text-align:center;font-size:large;font-weight:bold;" );
+	            p.before("{{field-on:author}}");
+	            p.after("{{field-off:author}}");
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("ded1") || p.attr("class").equalsIgnoreCase("ded2") ) {
+	            p.attr( "id", "dedication" );
+            	p.attr( "style", "font-style:italic;text-align:center;line-height:0.3;" );
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("copyright") ) {
+            	p.attr( "style", "font-size:small;text-align:center;" );
+	            p.before("{{field-on:copyright}}");
+	            p.after("{{field-off:copyright}}");
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("caption") ) {
+            	p.attr( "style", "font-size:small;text-align:center;" );
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("center") ) {
+            	p.attr( "style", "text-align:center;" );
+	            btags++;
+			} else if ( p.attr("class").equalsIgnoreCase("publogo") ) {
+            	p.attr( "style", "text-align:center;" );
+	            btags++;
+            } else if ( p.attr("class").equalsIgnoreCase("wslogo") || 
+                        p.attr("class").equalsIgnoreCase("coverimg") ) {
+            	p.remove();
+	            btags++;
+            } else if ( p.attr("class").equalsIgnoreCase("noind") ) {
+            	p.attr( "class", "" );
+            	p.attr( "style", "" );
+            	btags++;
+            } else if ( p.attr("class").equalsIgnoreCase("sig")  ||
+        				p.attr("class").equalsIgnoreCase("sig2") || 
+        				p.attr("class").equalsIgnoreCase("sig3") ) {
+            	p.attr( "class", "signature" );
+            	p.attr( "style", "text-align:right;font-style:italic;line-height:0.3;" );
+	            btags++;
+            } else if ( p.attr("class").equalsIgnoreCase("map")  ||
+        				p.attr("class").equalsIgnoreCase("mapcenter") || 
+        				p.attr("class").equalsIgnoreCase("chart") ) {
+            	p.attr( "style", "text-align:center;" );
+	            btags++;
+            } else if ( p.attr("class").equalsIgnoreCase("hang") ||
+        				p.attr("class").equalsIgnoreCase("hang2") ) {
+	        	p.attr( "style", "padding-left:22px; text-indent:-22px;" );
+	        	btags++;
+            } else if ( p.attr("class").equalsIgnoreCase("poem1") ||
+            			p.attr("class").equalsIgnoreCase("poem2") || 
+            			p.attr("class").equalsIgnoreCase("poem3") ||
+            			p.attr("class").equalsIgnoreCase("poem4")) {
+            	p.attr( "class", "poem" );
+            	p.attr( "style", "font-style:italic;text-indent:22px;line-height:0.3;" );
+            	btags++;
+			} else {
+				String cls = p.attr( "class" );
+				if ( !missed.contains( cls ) ) missed.add( cls );
+			}
+		}
+		if (btags > 0 ) System.out.println(btags + " paragraphs processed.");
+        if ( missed.size() > 0 ) System.out.println("  <P> classes not handled: " + missed.toString() );
+        
+        /*
+         * Process tables
+         */
+		Elements tables = doc.getElementsByTag("table");
+		if ( tables.size() > 0 ) {
+            Element e = new Element(Tag.valueOf("style"), "");
+            e.text("tr.nowrap { white-space:nowrap; } td { white-space:nowrap; }");
+			doc.body().before(e);
+		}
         
         /*
          * Check for IMG base URL
          */
         btags = 0;
         String imgpath = "";
+        /*
         Elements inputs = doc.getElementsByTag("input");
         for ( Element inp : inputs )
         {
@@ -155,7 +264,9 @@ public class LogosPrep {
             break;
           }
         }
+        */
         
+        imgpath = "file:///C:/Books/WORDsearch/Library/WelwynCmy/";
         if ( imgpath == null ) {
           System.out.println("Image base path is NULL!");
         } else {
@@ -163,8 +274,8 @@ public class LogosPrep {
           for ( Element img : imgs )
           {
             String rel = img.attr( "src" );
-            img.attr( "src", imgpath + rel );
-            System.out.println("   IMAGE: " + img.attr( "src" ));
+            img.attr( "src", imgpath + this.getImgDirWelwyn(rel) + "/" + rel );
+            //System.out.println("   IMAGE: " + img.attr( "src" ));
             btags++;
           }
           if (btags > 0 ) System.out.println( btags + " images converted.");
@@ -184,6 +295,8 @@ public class LogosPrep {
 					if ( a.startsWith("page") ) {
 						String page = a.substring(4);
 						link.replaceWith(new TextNode("[[@Page:" + page + "]]", ""));
+					} else if ( a.equals("essm") ) {
+						link.remove();
 					}
 				} else if ( !link.attr("href").isEmpty() ) {
 				  try {
@@ -207,29 +320,41 @@ public class LogosPrep {
 		 */
 		String fn = input.getAbsolutePath();
 		File output = new File( fn.substring(0, fn.lastIndexOf('.')) + "_[modified].html");
-		FileWriter fw = new FileWriter(output);
-		String[] lines = doc.toString().split("\r\n|\r|\n");
-		String nl = System.getProperty("line.separator");
-		int excluded = 0;
+		BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+		bw.write(doc.toString());
+		bw.close();
 
-		for (int i = 0; i < lines.length; i++) {
-			String s = lines[i].trim(); 
-			if ( s.startsWith("Version:1.0 StartHTML:") || s.startsWith("<!--EndFragment-->") ) {
-				//System.out.println( i + ": " + lines[i] );
-				excluded++;
-			} else {
-				fw.write( lines[i] );
-				fw.write( nl );
-			}
-		}
-        System.out.println("Excluded " + excluded + " lines.");
-		
-        fw.close();
-        System.out.println("Converted file: " + output.getName() );
+		System.out.println("Converted file: " + output.getName() );
         System.out.println("Original file size: " + input.length() );
         System.out.println("Converted file size: " + output.length() );
 	}
 	
+	private String getImgDirWelwyn( String img )
+	{
+		String series = "Welwyn";
+		String str = img.substring(img.lastIndexOf('/') + 1);
+		str = str.substring(0, str.indexOf('.'));
+		int dash = str.indexOf('-');
+		if ( dash > 2 ) str = str.substring(0, dash);
+		int w = str.indexOf(series);
+		if ( w == -1 ) {
+			if ( str.equalsIgnoreCase("EvangelicalPress") || 
+				 str.equalsIgnoreCase("wslogo") ||
+				 str.equalsIgnoreCase("numberscoverimg") ||
+				 str.equalsIgnoreCase("coverimage") ) {
+				str = "Rev";
+			} else if ( str.startsWith("2-2Samuel") ) {
+				str = "2Samuel";
+			}
+		} else if ( w == 0 ) {
+			str = str.substring(series.length());
+		} else {
+			str = str.substring(0, w);
+		}
+		//System.out.println("  (imgDir: " + str + ")");
+		return str;
+	}
+
 	private String getLogosRef( String title )
 	{
 		/*
@@ -267,7 +392,7 @@ public class LogosPrep {
 		String[] a = {"genesis", "gen", "ge", "gn", "exodus", "exo", "ex", "exod", "leviticus", "lev", "le", "lv", "numbers", "num", "nu", "nm", "nb", "deuteronomy", "deut", "dt", "joshua", "josh", "jos", "jsh", "judges", "judg", "jdg", "jg", "jdgs", "ruth", "rth", "ru", "1 samuel", "1 sam", "1 sa", "1samuel", "1s", "i sa", "1 sm", "1sa", "i sam", 
 				"1sam", "i samuel", "1st samuel", "first samuel", "2 samuel", "2 sam", "2 sa", "2s", "ii sa", "2 sm", "2sa", "ii sam", "2sam", "ii samuel", "2samuel", "2nd samuel", "second samuel", "1 kings", "1 kgs", "1 ki", "1k", "i kgs", "1kgs", "i ki", "1ki", "i kings", "1kings", "1st kgs", "1st kings", "first kings", "first kgs", "1kin", "2 kings", "2 kgs", "2 ki", "2k", "ii kgs", "2kgs", "ii ki", "2ki", "ii kings", 
 				"2kings", "2nd kgs", "2nd kings", "second kings", "second kgs", "2kin", "1 chronicles", "1 chron", "1 ch", "i ch", "1ch", "1 chr", "i chr", "1chr", "i chron", "1chron", "i chronicles", "1chronicles", "1st chronicles", "first chronicles", "2 chronicles", "2 chron", "2 ch", "ii ch", "2ch", "ii chr", "2chr", "ii chron", "2chron", "ii chronicles", "2chronicles", "2nd chronicles", "second chronicles", "ezra", "ezr", "nehemiah", "neh", "ne", "esther", "esth", "es", 
-				"job", "jb", "psalm", "pslm", "ps", "psalms", "psa", "psm", "pss", "proverbs", "prov", "pr", "prv", "pro", "ecclesiastes", "eccles", "ec", "ecc", "qoh", "qoheleth", "song of solomon", "song", "so", "canticle of canticles", "canticles", "song of songs", "sos", "isaiah", "isa", "is", "jeremiah", "jer", "je", "jr", "lamentations", "lam", "la", "ezekiel", "ezek", "eze", "ezk", 
+				"job", "jb", "psalm", "pslm", "ps", "psalms", "psa", "psm", "pss", "proverbs", "prov", "pr", "prv", "pro", "ecclesiastes", "eccles", "ec", "ecc", "qoh", "qoheleth", "song of solomon", "song", "so", "canticle of canticles", "canticles", "song of songs", "ss", "sos", "isaiah", "isa", "is", "jeremiah", "jer", "je", "jr", "lamentations", "lam", "la", "ezekiel", "ezek", "eze", "ezk", 
 				"daniel", "dan", "da", "dn", "hosea", "hos", "ho", "joel", "joe", "jl", "amos", "am", "obadiah", "obad", "ob", "jonah", "jnh", "jon", "micah", "mic", "nahum", "nah", "na", "habakkuk", "hab", "zephaniah", "zeph", "zep", "zp", "haggai", "hag", "hg", "zechariah", "zech", "zec", "zc", "malachi", "mal", "ml", "matthew", "matt", 
 				"mt", "mark", "mrk", "mk", "mr", "luke", "luk", "lk", "john", "jn", "jhn", "acts", "ac", "romans", "rom", "ro", "rm", "1 corinthians", "1 cor", "1 co", "i co", "1co", "i cor", "1cor", "i corinthians", "1corinthians", "1st corinthians", "first corinthians", "2 corinthians", "2 cor", "2 co", "ii co", "2co", "ii cor", "2cor", "ii corinthians", "2corinthians", "2nd corinthians", "second corinthians", "galatians", "gal", 
 				"ga", "ephesians", "ephes", "eph", "philippians", "phil", "php", "colossians", "col", "1 thessalonians", "1 thess", "1 th", "i th", "1th", "i thes", "1thes", "i thess", "1thess", "i thessalonians", "1thessalonians", "1st thessalonians", "first thessalonians", "2 thessalonians", "2 thess", "2 th", "ii th", "2th", "ii thes", "2thes", "ii thess", "2thess", "ii thessalonians", "2thessalonians", "2nd thessalonians", "second thessalonians", "1 timothy", "1 tim", "1 ti", "i ti", "1ti", "i tim", 
